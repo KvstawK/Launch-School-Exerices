ec95ca548d05e34b8ffa87599739d8f5091abfe7
KostasK <kvstaw4@mail.com>
2023-12-19 11:32:35 +0100

    car-loan-finished

--- Ruby/Programming Foundations with Ruby - Basics/Lesson 2 - Small Programs/car_loan_calculator.rb
+++ Ruby/Programming Foundations with Ruby - Basics/Lesson 2 - Small Programs/car_loan_calculator.rb
@@ -14,10 +14,17 @@ def user_input(text, convert_to_integer: false)
   end
 end
 
-loan_amount = user_input("Please enter the desired loan amount. Use a full stop '.' to represent the decimal point and format the number, for example, '2.500' or '4.560' for hundreds and tens.")
-loan_arp = user_input("Please enter the Annual Percentage Rate. Use a full stop '.' to represent the decimal point and format the number, for example, '6.5' or '10.54.: ")
-loan_duration = user_input("Enter the loan duration in months, please: ", convert_to_integer: true)
+def calculate(loan_amount, loan_arp, loan_duration)
+  annual_interest_rate = loan_arp / 100.0
+  monthly_interest_rate = annual_interest_rate / 12.0
+  monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration)))
+end
+
+loan_amount = user_input("Please enter the desired loan amount. Use a full stop '.' to represent the decimal point and format the number, for example, '2.500' or '4.560' for hundreds and tens: ")
+loan_arp = user_input("Please enter the Annual Percentage Rate. Use a full stop '.' to represent the decimal point and format the number, for example, '6.5' or '10.54: ")
+loan_duration = user_input("Please enter the loan duration in months. Only integer numbers allowed: ", convert_to_integer: true)
 
 puts "Loan Amount: #{loan_amount}"
 puts "Annual Percentage Rate: #{loan_arp}"
 puts "Loan Duration: #{loan_duration} months"
+puts "Your monthly payment for the loan you required is: #{calculate(loan_amount, loan_arp, loan_duration)}"
